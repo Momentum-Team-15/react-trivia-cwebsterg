bec9ab5cde7acfcfd32f37730110b6ad2c80f710
Webster Grimes <cwebsterg@mac.com>
2022-11-09 11:48:16 -0500

    min button styling & beg. additional components

--- scratch.js
+++ scratch.js
@@ -1,17 +1,4 @@
-import 'bulma/css/bulma.min.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
-const URL='https://opentdb.com/api_category.php'
 
-
-function App () {
-    const [triviaDB, setTriviaDB] = useState([])
-    useEffect(() => {
-        axios.get(URL)
-        .then(data => setCategories(response.data))
-    },[]
-}
 // response is just a string, could be e.g., "data" but the axios wrapper is called data (is this globally true?)
 // if there was permission issue => create headers: {with authorization: information/token/whatever is needed I guess??}
 // empty dependency array stops the infinite loop
@@ -32,16 +19,3 @@ function App () {
 
 
 
-
-### What needs to happen?
-
-- [ ] User arrives at homepage and is presented with a "list" of categories
-- [ ] User sees a welcome message with some type of instructions
-- [ ] User can select category
-- [ ] User is shown a question
-- [ ] User is able to select the answer
-  - [ ] answers are shuffled so that the correct answer is not always in the same index
-  - [ ] 
-  - [ ] 
-- [ ] User either gets correct/incorrect
-- [ ] 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import 'bulma/css/bulma.min.css'
+// import 'bulma/css/bulma.min.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css';
-const URL='https://opentdb.com/api_php?amount=10&category='
+import './App.css'
+const catUrl='https://opentdb.com/api_category.php'
+let questionURL='https://opentdb.com/api.php?type=multiple&amount=10&category='
 
 
 function App () {
-  const [triviaDB, setTriviaDB] = useState([])
-
+  const [categories, setCategories] = useState([])
+  const [selectedCategoryId, setSelectedCategoryID] = useState(null)
+  
+  const handleSelect = () => {
+    setSelectedCategoryID(topic)
+    console.log(topic)
+  }
   useEffect(() => {
-    axios.get(URL)
-    .then(data => setCategories(response.data))
-    },[]
+    axios.get(catUrl)
+    .then(res => setCategories(res.data.trivia_categories))
+    },[])
+    // empty dependency array [] stops the infinite loop
+
+  // useEffect(() => {
+  //   axios.get
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Trial by Trivia</h1>
       </header>
-      <button className="category"></div>
+      <div className="cat-container"></div>
+        {categories.map((topic) => ( 
+          <button className="category" onClick={handleSelect}><h4>{topic.name}</h4></button>
+        ))}
     </div>
   );
 }
