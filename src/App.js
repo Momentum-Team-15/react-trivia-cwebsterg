8e1006b4c1598172ab6fb80d9ded69003f719aed
Webster Grimes <cwebsterg@mac.com>
2022-11-13 22:49:24 -0500

    error resolved, still no score calculated/shown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getCategories } from "./components/requests";
 import { Categories } from "./components/categories";
 import { Questions } from "./components/questions";
